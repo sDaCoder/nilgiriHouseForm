893caf846d3071e9436ef4e5d326667c1cd45c2c
sDaCoder <supravadutta_sDa@outlook.com>
2024-11-09 12:21:41 +0530

    editing README

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
-# IIT Madras Nilgiri House Projects(2024-25)
+## IIT Madras Nilgiri House Projects(2024-25)
 
 Projects made by the Web Administrators of Nilgiri House, IIT Madras(2024-25)
+
+
+## Authors
+
+- [@sDaCoder](https://www.github.com/sDaCoder)
+
+- [@itsrahul_8](https://github.com/rahuldk1105)
+
+- [@AnushkaBhujade](https://github.com/AnushkaBhujade)
--- server/main.py
+++ server/main.py
@@ -1,2 +1,27 @@
 # Code for Backend
 # Packages installed -> FastAPI and uvicorn
+
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+class Item(BaseModel):
+    name: str
+    description: str | None = None
+    price: float
+    tax: float | None = None
+
+app = FastAPI()
+
+@app.get("/")
+def root():
+    return {"message": "Hello World"}
+
+@app.post("/item")
+async def get_items(I: Item):
+    dic = {"message": "Success", **I.dict()}
+    print(dic)
+    return dic
+
+@app.get("/{name}")
+def hello(name: str):
+    return {"message": f"Hello {name}"}
